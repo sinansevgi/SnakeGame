f49470199479e31a0f477c184deb7ca27ec6f1d6
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-04-08 17:13:34 +0300

    Add apple and snake classes

--- main.js
+++ main.js
@@ -6,27 +6,35 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+/******/
+(() => { // webpackBootstrap
+  /******/
+  var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ (() => {
+    /***/
+    "./src/index.js":
+      /*!**********************!*\
+        !*** ./src/index.js ***!
+        \**********************/
+      /***/
+      (() => {
 
-eval("\n\n//# sourceURL=webpack://SnakeGame/./src/index.js?");
+        eval("\n\n//# sourceURL=webpack://SnakeGame/./src/index.js?");
 
-/***/ })
+        /***/
+      })
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
-/******/ })()
-;
+    /******/
+  });
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval devtool is used.
+  /******/
+  var __webpack_exports__ = {};
+  /******/
+  __webpack_modules__["./src/index.js"]();
+  /******/
+  /******/
+})();
